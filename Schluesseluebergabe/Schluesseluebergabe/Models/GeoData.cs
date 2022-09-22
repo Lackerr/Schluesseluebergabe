e36e7c2c62ceacf83380c350a9f7ba60373b947b
Lackerr <elias.kist@web.de>
2022-09-22 21:42:00 +0200

    view angepasst / Datum ohne Zeit drucken

--- Schluesseluebergabe/Schluesseluebergabe/Models/GeoData.cs
+++ Schluesseluebergabe/Schluesseluebergabe/Models/GeoData.cs
@@ -7,14 +7,17 @@ namespace Schluesseluebergabe.Models
     {
         public string? City { get; set; }
         public DateTime Date { get; set; }
+        public string DateString { get => Date.ToShortDateString(); }
+
         private readonly ConfigManager _cfgManager;
+
 
         public GeoData()
         {
             _cfgManager = ConfigManager.Instance;
 
 
-            Date = DateTime.Now.Date;
+            Date = DateTime.Now;
             City = _cfgManager.GetConfig().City;
         }
     }
